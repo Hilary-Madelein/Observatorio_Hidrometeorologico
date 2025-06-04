6ca35ced1292e33a056b35ccf32ab2d1e6781101
Hilary-Madelein <hilary.calva@unl.edu.ec>
2025-06-04 12:10:12 -0500

    Cambio build

--- observatorio_web/src/App.js
+++ observatorio_web/src/App.js
@@ -1,33 +1,40 @@
+// src/App.js
 import React from 'react';
 import './App.css';
 import Principal from './fragments/Principal';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Login from './fragments/Login';
 import ListaMicrocuencas from './fragments/ListaMicrocuencas';
 import ListaEstaciones from './fragments/ListaEstaciones';
 import ListaVariables from './fragments/ListaVariables';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import { getRoles, getToken } from './utils/SessionUtil';
-import mensajes from './utils/Mensajes';
 import 'boxicons/css/boxicons.min.css';
 import Perfil from './fragments/Perfil';
 import ListaAdministradores from './fragments/ListaAdministradores';
 
-function App() {
+import { getRoles, getToken } from './utils/SessionUtil';
+import mensajes from './utils/Mensajes';
 
+function App() {
   const MiddewareSesion = ({ children, requiredRoles }) => {
     const autenticado = getToken();
     const roles = getRoles() || [];
 
     if (!autenticado) {
-      return <Navigate to='/admin' />;
+      return <Navigate to="/admin" />;
     }
 
     if (requiredRoles && requiredRoles.length > 0) {
-      const hasRequiredRole = roles.some(role => requiredRoles.includes(role.nombre));
+      const hasRequiredRole = roles.some(role =>
+        requiredRoles.includes(role.nombre)
+      );
       if (!hasRequiredRole) {
-        mensajes("No tienes el rol necesario para acceder a esta página.", "error", "Acceso Denegado");
-        return <Navigate to='/admin' />;
+        mensajes(
+          "No tienes el rol necesario para acceder a esta página.",
+          "error",
+          "Acceso Denegado"
+        );
+        return <Navigate to="/admin" />;
       }
     }
 
@@ -35,20 +42,54 @@ function App() {
   };
 
   return (
-    <BrowserRouter basename="/hid">
-      <Routes>
-        <Route path='/principal/monitorizacion' element={<Principal />} />
-        <Route path='*' element={<Navigate to='/principal/monitorizacion' />} />
+    <Routes>
+      {/** Ruta principal de monitoreo */}
+      <Route path="/principal/monitorizacion" element={<Principal />} />
+      <Route path="*" element={<Navigate to="/principal/monitorizacion" />} />
 
-        {/** RUTAS ADMINISTRATIVAS */}
-        <Route path='/admin' element={<Login />} />
-        <Route path='/admin/perfil' element={<MiddewareSesion ><Perfil /></MiddewareSesion>} />
-        <Route path='/principal/admin' element={<MiddewareSesion ><ListaMicrocuencas /></MiddewareSesion>} />
-        <Route path='/estaciones/:external_id' element={<MiddewareSesion ><ListaEstaciones /></MiddewareSesion>} />
-        <Route path='/principal/variable' element={<MiddewareSesion ><ListaVariables /></MiddewareSesion>} />
-        <Route path='/principal/gestionar/admin' element={<MiddewareSesion ><ListaAdministradores /></MiddewareSesion>} />
-      </Routes>
-    </BrowserRouter>
+      {/** Rutas administrativas */}
+      <Route path="/admin" element={<Login />} />
+      <Route
+        path="/admin/perfil"
+        element={
+          <MiddewareSesion>
+            <Perfil />
+          </MiddewareSesion>
+        }
+      />
+      <Route
+        path="/principal/admin"
+        element={
+          <MiddewareSesion>
+            <ListaMicrocuencas />
+          </MiddewareSesion>
+        }
+      />
+      <Route
+        path="/estaciones/:external_id"
+        element={
+          <MiddewareSesion>
+            <ListaEstaciones />
+          </MiddewareSesion>
+        }
+      />
+      <Route
+        path="/principal/variable"
+        element={
+          <MiddewareSesion>
+            <ListaVariables />
+          </MiddewareSesion>
+        }
+      />
+      <Route
+        path="/principal/gestionar/admin"
+        element={
+          <MiddewareSesion>
+            <ListaAdministradores />
+          </MiddewareSesion>
+        }
+      />
+    </Routes>
   );
 }
 
